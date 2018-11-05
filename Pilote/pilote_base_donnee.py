a605d5069c7d989f248808c27c5a73e9f78ca18f
GuillaumeClementPolytech <43247385+GuillaumeClementPolytech@users.noreply.github.com>
2018-11-05 16:15:02 +0100

    Update pilote_base_donnee.py

--- Pilote/pilote_base_donnee.py
+++ Pilote/pilote_base_donnee.py
@@ -1,26 +1,19 @@
 import xlrd # faire pip install xlutil
 
-# ouverture du fichier Excel 
-
+## ouverture du fichier Excel 
 # wb = xlrd.open_workbook('baseflor.xlsx')
- 
-# feuilles dans le classeur
-
+## feuilles dans le classeur
 # print wb.sheet_names()
- 
-# lecture des données dans la première feuille
+## lecture des données dans la première feuille
 # sh = wb.sheet_by_name(u'NomFeuille1')
 # for rownum in range(sh.nrows): # pour chaque ligne
 #     print sh.row_values(rownum) # lire la ligne
-
-# lecture par colonne
+## lecture par colonne
 # colonne1 = sh.col_values(0)
 # print colonne1
- 
 # colonne2=sh.col_values(1)
 # print colonne2
- 
-# extraction d'un élément particulier
+## extraction d'un élément particulier
 # print colonne1[1],colonne2[1]
 
 def afficheContenuCase (a,b): #la base de donnée à besoin d'être dans le même dossier que ce fichier, a est la ligne, b la colonne
