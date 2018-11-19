d2600ab3b1e76c7bbd4c24eb13650b87bf1105bd
Debatent <alexandreadrien@orange.fr>
2018-11-19 19:35:44 +0100

    Gestion erreur d un trop grand nombre de tentative de lecture du capteur

--- programme_principal.py
+++ programme_principal.py
@@ -8,4 +8,9 @@ pin_buzzer=1
 pin_lum=2
 
 while True:
-    
+    try:
+        a=moyenne_eau(pin_hum_sol_prof,pin_hum_sol_surf)
+    except TentativeError:
+        print("Le ou les capteurs d'humidité n'ont pas pu être lu")
+        print("Verifiez si le capteur d'humidité de profondeur est branché à la prise A"+pin_hum_sol_prof)
+        print("Verifiez si le capteur d'humidité de surface est branché à la prise A"+pin_hum_sol_prof)
