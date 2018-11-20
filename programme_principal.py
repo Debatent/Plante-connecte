8585780fca3ccdd00901ab7f9384b455b6e4abc8
Debatent <alexandreadrien@orange.fr>
2018-11-20 14:49:36 +0100

    Ajout de fonction

--- programme_principal.py
+++ programme_principal.py
@@ -4,13 +4,39 @@ from Pilote/pilote_fonction.py import  *
 pin_hum_sol_surf=0
 pin_hum_sol_prof=1
 pin_temp=0
-pin_buzzer=1
 pin_lum=2
+pin_buzzer=1
+
+
+eau=[]
+lumiere=[]
+humidite=[]
+temperature=[]
+
 
 while True:
     try:
-        a=moyenne_eau(pin_hum_sol_prof,pin_hum_sol_surf)
+        eau=ajout_eau(pin_hum_sol_prof,pin_hum_sol_surf,eau)
     except TentativeError:
         print("Le ou les capteurs d'humidité n'ont pas pu être lu")
-        print("Verifiez si le capteur d'humidité de profondeur est branché à la prise A"+pin_hum_sol_prof)
-        print("Verifiez si le capteur d'humidité de surface est branché à la prise A"+pin_hum_sol_prof)
+        print("Vérifiez si le capteur d'humidité de profondeur est branché à la prise A"+pin_hum_sol_prof)
+        print("Vérifiez si le capteur d'humidité de surface est branché à la prise A"+pin_hum_sol_prof)
+    else:
+        print("Valeur de l'eau ajoutée")
+
+
+    try:
+        lumiere=ajout_lumiere(pin_lum,lumiere)
+    else:
+        print("Valeur de luminosité ajoutée")
+
+
+    try:
+        humidite=ajout_humidite(pin_temp, humidite)
+    else:
+        print("Valeur d'humiditée ajoutée")
+
+    try:
+        temperature=ajout_temperature(pin_temp,temperature)
+        else:
+            print("Valeur de température ajoutée")
