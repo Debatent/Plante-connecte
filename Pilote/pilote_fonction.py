52a020c58c796888f350a3e3751ed23594ff79ec
Debatent <alexandreadrien@orange.fr>
2018-11-22 15:15:59 +0100

    finition fonction ajout_eau

--- Pilote/pilote_fonction.py
+++ Pilote/pilote_fonction.py
@@ -1,4 +1,11 @@
 from Basique/pilote_basique_capteur.py import *
 
-def moyenne_eau(pin_surf,pin_prof):
+def ajout_eau(pin_surf, pin_prof, liste, n):
     a=lecture_humidite_sol (pin_surf)
+    b=lecture_humidite_sol (pin_prof)
+    c=(a+b)/2
+    liste.append(c)
+    if len(liste)>n:
+        return liste[1:]
+    else:
+        return liste
