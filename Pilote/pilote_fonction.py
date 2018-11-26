6cee118ad8a5a1ddd174ec24e832c9870a7b5788
Debatent <alexandreadrien@orange.fr>
2018-11-26 15:31:47 +0100

    sauvegarde charger

--- Pilote/pilote_fonction.py
+++ Pilote/pilote_fonction.py
@@ -1,5 +1,37 @@
 from Basique/pilote_basique_capteur.py import *
 
+################### Pilote de sauvegarde/chargement ###########################
+
+def sauvegarder(liste, nom_fichier):
+    texte=""
+    virgule = ""
+    for i in liste:
+        texte=text+ virgule +str(i)
+        virgule = ","
+
+    try:
+        fichier = open (nom_fichier,w)
+        fichier.writeline (texte)
+    finally:
+        fichier.close()
+
+def charger (nom_fichier):
+    try:
+        fichier =open (nom_fichier,r)
+        texte=fichier.readline()
+    else:
+        print (fichier + " chargé")
+    finally:
+        fichier.close()
+    texte=texte.split(",")]
+    resultat=[]
+    for i in texte:
+        resultat.append(int(i))
+    return resultat
+
+####################### fonction ###############################
+
+
 def ajout_eau(pin_surf, pin_prof, liste, n):
     a=lecture_humidite_sol (pin_surf)
     b=lecture_humidite_sol (pin_prof)
