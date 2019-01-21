86cd207a70f30182a83b602fe3499cfa3ee8f460
GuillaumeClementPolytech <guillaume.clement@etu.umontpellier.Fr>
2019-01-21 17:52:45 +0100

    crontab fait

--- crontab.sh
+++
@@ -1,8 +0,0 @@
-# a faire dans le terminal
-# >crontab -e
-# Cela amene vers un editeuret un fichier de job réguliers
-# rajouter :
-# 0-59/5 * * * * "python /home/plante/Plante-connecte/programme_principal.py"
-#
-# et donner tout les droits e tout le monde sur ce fichier
-
